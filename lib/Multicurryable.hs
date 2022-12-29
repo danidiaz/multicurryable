2c6214273447aba265250482d2ada568e428292c
Daniel Díaz <diaz_carrete@yahoo.com>
2022-12-29 20:53:11 +0100

    compiles

--- lib/Multicurryable.hs
+++ lib/Multicurryable.hs
@@ -1,35 +1,56 @@
-{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Multicurryable where
 
+import Data.Functor.Identity
+import Data.Kind
 import Data.SOP.NP
 import Data.SOP.NS
-import Data.Kind
-import Data.Functor.Identity
 
 type Multicurryable :: (Type -> Type -> Type) -> [Type] -> Type -> Type -> Constraint
-class Multicurryable f items a curried | f items a -> curried , f curried -> items, f curried -> a where
-    type UncurriedCollection f items :: Type
-    multiuncurry :: curried -> f (UncurriedCollection f items) a
-    multicurry :: f (UncurriedCollection f items) a -> curried
+class Multicurryable f items a curried | f items a -> curried where
+  type UncurriedCollection f :: [Type] -> Type
+  multiuncurry :: curried -> f (UncurriedCollection f items) a
+  multicurry :: f (UncurriedCollection f items) a -> curried
 
 type family IsFunction f :: Bool where
-    IsFunction (_ -> _) = 'True
-    IsFunction _ = 'False
+  IsFunction (_ -> _) = 'True
+  IsFunction _ = 'False
 
 instance
-    MulticurryableF (IsFunction curried) items a curried =>
-        Multicurryable (->) items a curried where
-    type UncurriedCollection (->) items = NP  Identity items
-    multiuncurry = multiuncurryF @(IsFunction curried)
-    multicurry = multicurryF @(IsFunction curried)
+  MulticurryableF (IsFunction curried) items a curried =>
+  Multicurryable (->) items a curried
+  where
+  type UncurriedCollection (->) = NP Identity
+  multiuncurry = multiuncurryF @(IsFunction curried)
+  multicurry = multicurryF @(IsFunction curried)
 
-class MulticurryableF (b :: Bool) items a curried | items a -> curried , curried -> items, curried -> a where
-    multiuncurryF :: curried -> NP Identity items -> a
-    multicurryF :: (NP Identity items -> a) -> curried
+class MulticurryableF (b :: Bool) items a curried 
+  -- | b items a -> curried, b curried -> items, b curried -> a where
+   | items a -> curried
+    where
+  multiuncurryF :: curried -> NP Identity items -> a
+  multicurryF :: (NP Identity items -> a) -> curried
+
+instance MulticurryableF 'False '[] a a where
+  multiuncurryF = \a -> \Nil -> a
+  multicurryF = \f -> f Nil
+
+instance 
+    MulticurryableF (IsFunction curried) rest tip curried =>
+    MulticurryableF 'True (i ': rest) tip (i -> curried) where
+  multiuncurryF f (Identity x :* rest) =
+     multiuncurryF @(IsFunction curried) @rest @tip @curried (f x) rest
+  multicurryF f i =
+     multicurryF @(IsFunction curried) @rest @tip @curried $ \rest -> f (Identity i :* rest)
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeOperators #-}
 module Main (main) where
+
+import Multicurryable
+
+type Fun0 = Int
+type Fun0b = IO Int
+type Fun1 = Bool -> Int
+type Fun1b = Bool -> IO Int
+type Fun2 = Char -> Bool -> Int
+type Fun2b = Char -> Bool -> IO Int
+
+-- fun0 = multiuncurry @(->)
+
 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented."
